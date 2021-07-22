357dea45cac39388624f2946b0b00988f5c915c4
Piotr Pośpiech <piotr.pospiech@vazco.eu>
2021-07-22 12:02:54 +0200

    Update accounts-base to v2.0.0

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
   name: "teebly:impersonate",
   summary: "Impersonate users in Meteor",
-  version: "0.2.2",
+  version: "0.2.3",
   git: "https://github.com/Teebly/meteor-impersonate.git",
 });
 
 Package.onUse(function (api, where) {
 
   api.use([
-    "accounts-base@1.6.0",
+    "accounts-base@2.0.0",
     "reactive-var@1.0.11",
     "templating@1.3.2",
     "gwendall:body-events@0.1.6",
